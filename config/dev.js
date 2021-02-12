39be3139ed40c6cf6a490e6ac5a9c0173a39514e
dd6679 <35446851+dd6679@users.noreply.github.com>
2021-02-13 03:56:49 +0900

    비밀정보 보호

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://nk:abcd1234@cluster.i4bsm.mongodb.net/db?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -1,16 +1,43 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = 4000
+const bodyParser = require('body-parser');
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://nk:abcd1234@cluster.i4bsm.mongodb.net/db?retryWrites=true&w=majority', {
-    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
-}).then(()=>console.log('MongoDB Connected...'))
+const config = require("./config/key");
+
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+const { request } = require('express');
+mongoose.connect(config.mongoURI, {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
-  res.send('노드 강의 듣고 있어욤')
+  res.send('새해복 많이 받으세요')
+})
+
+app.post('/register', (req, res) => {
+  // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터베이스에 넣어준다.
+  const user = new User(req.body)
+
+  // 몽고db에서 오는 메서드
+  user.save((err, userInfo) => {
+    if (err)
+      return res.json({ seccess: false, err })
+    return res.status(200).json({
+      seccess: true
+    })
+  })
 })
 
 app.listen(port, () => {
